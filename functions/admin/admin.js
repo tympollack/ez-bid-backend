ff54995da449415086e5538d47533de9ab818604
tympollack <tpollack@gaig.com>
2018-12-26 10:13:32 -0500

    admin tests

--- functions/admin/admin.js
+++ functions/admin/admin.js
@@ -9,6 +9,7 @@ const routes = require('express').Router()
 
 routes.post('/badAuctionNumDedupe', badAuctionNumDedupe)
 routes.get('/findNewAuctions', findNewAuctions)
+routes.get('/findNewItems', findNewItems)
 routes.get('/test', test)
 routes.get('/otherTest', otherTest)
 routes.get('/testFindAuctions', testFindAuctions)
@@ -33,78 +34,29 @@ async function badAuctionNumDedupe(req, res) {
 }
 
 async function otherTest(req, res) {
-    psFuncs.findNewAuctions()
-        .then(resp => { res.json(resp) })
-        .catch(err => { res.json(err) })
+
 }
 
 async function test(req, res) {
-    psFuncs.findNewItems()
-        .then(resp => {
-            if (typeof resp === 'string') console.log(resp)
-            res.json(resp)
-        })
-        .catch(err => { res.status(500).json(err) })
+
 }
 
 async function findNewAuctions(req, res) {
-    console.log('getting user session from firestore')
-    let opts = await fsFuncs.getFsUserSession(vars.FS_SERVICE_ACCOUNT_ID)
-    if (!opts) {
-        console.log('failed getting user session')
-        res.status(500).json(opts)
-        return
-    }
-    opts.db = db
-    opts.skipLogin = true
+    psFuncs.findNewAuctions()
+        .then(resp => {
+            logIfString(resp)
+            res.json(resp)
+        })
+        .catch(err => { res.json(err) })
+}
 
-    console.log('preparing crawl for auctions')
-    const highestGoodAuction = await fsFuncs.findHighestGoodAuction()
-    const highestGoodAuctionNum = highestGoodAuction[vars.FS_AUCTION_AUCTION_NUMBER] || 0
-    const badAuctionNums = await fsFuncs.getUnusedAuctionNumbers() || []
-    const highestBadAuctionNum = badAuctionNums.sort((a, b) => { return b - a })[0]
-    const isHighestBadNumberTooHigh = highestBadAuctionNum >= highestGoodAuctionNum + vars.PS_FIND_AUCTIONS_AMOUNT
-    const isHighestGoodAuctionTooOld = (highestGoodAuction[vars.FS_AUCTION_END_DATE] || {})._seconds * 1000 < new Date()
-    const startNum = (isHighestBadNumberTooHigh && isHighestGoodAuctionTooOld) ? highestBadAuctionNum : highestGoodAuctionNum
-
-    const auctionNumsToGet = []
-    let i = 1
-    while (auctionNumsToGet.length < vars.PS_FIND_AUCTIONS_AMOUNT) {
-        const testNum = startNum + i
-        if (badAuctionNums.indexOf(testNum) === -1) auctionNumsToGet.push(testNum)
-        i++
-    }
-
-    const auctionInfos = await puppetFuncs.crawlAuctionInfo(auctionNumsToGet, opts)
-    const goodInfos = []
-    let shouldUpdateBadNums = false
-    auctionInfos.forEach(info => {
-        const num = info[vars.FS_AUCTION_AUCTION_NUMBER]
-        if (info.error) {
-            console.log('Unable to crawl auction at this time.', num || '', info.error)
-            return
-        }
-        if (!info.name) {
-            console.log('bad auction num', num)
-            if (badAuctionNums.indexOf(num) === -1) {
-                badAuctionNums.push(num)
-                shouldUpdateBadNums = true
-            }
-            return
-        }
-
-        info[vars.FS_AUCTION_ADD_DATE] = new Date()
-        info[vars.FS_AUCTION_ITEM_LIST] = []
-        info[vars.FS_AUCTION_ITEMS_CRAWLED] = false
-        info[vars.FS_AUCTION_SANITIZED] = false
-        goodInfos.push(info)
-        console.log('auctionInfo set for', num)
-    })
-
-    if (shouldUpdateBadNums) fsFuncs.setUnusedAuctionNumbers(badAuctionNums)
-    if (goodInfos.length) fsFuncs.addAuctions(goodInfos)
-
-    res.json(auctionInfos)
+async function findNewItems(req, res) {
+    psFuncs.findNewItems()
+        .then(resp => {
+            logIfString(resp)
+            res.json(resp)
+        })
+        .catch(err => { res.status(500).json(err) })
 }
 
 async function testFindAuctions(req, res) {
@@ -162,4 +114,10 @@ async function testFindAuctions(req, res) {
     ret.badNumbers.sort()
 
     res.status(200).json(ret)
+}
+
+/////////////////////////////////////////////////////////////////////
+
+function logIfString(r) {
+    if (typeof r === 'string') console.log(r)
 }
--- functions/pubsub/listeners.js
+++ functions/pubsub/listeners.js
@@ -3,14 +3,20 @@ const { functions, vars } = module.parent.shareable
 
 exports.findNewAuctions = functions.runWith(vars.PUPPETEER_OPTS).pubsub.topic(vars.PS_TOPICS.findNewAuctions).onPublish(message => {
     console.log('Processing queue:', vars.PS_TOPICS.findNewAuctions)
-    psFuncs.findNewAuctions().catch(err => { console.log(err) })
+    psFuncs.findNewAuctions()
+        .then(resp => { logIfString(resp) })
+        .catch(err => { console.log(err) })
 })
 
 exports.loginQueue = functions.runWith(vars.PUPPETEER_OPTS).pubsub.topic(vars.PS_TOPICS.loginqueue).onPublish(message => {
     console.log('Processing queue:', vars.PS_TOPICS.loginqueue)
-    console.log(message)
-    if (message.data) {
-        const dataString = Buffer.from(message.data, 'base64').toString()
-        console.log(`Message Data: ${dataString}`)
-    }
+    psFuncs.findNewItems()
+        .then(resp => { logIfString(resp) })
+        .catch(err => { console.log(err) })
 })
+
+/////////////////////////////////////////////////////////////////////
+
+function logIfString(r) {
+    if (typeof r === 'string') console.log(r)
+}
